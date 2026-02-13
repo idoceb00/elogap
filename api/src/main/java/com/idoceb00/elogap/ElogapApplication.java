f9b028295cbadbb03f94fbf10204ae170610461a
idoceb00 <idoceb00@estudiantes.unileon.es>
2026-02-13 17:09:57 +0100

    feat(api): initialize Spring Boot backend with /health endpoint

---
+++ api/src/main/java/com/idoceb00/elogap/ElogapApplication.java
@@ -0,0 +1,11 @@
+package com.idoceb00.elogap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElogapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElogapApplication.class, args);
+    }
+}
