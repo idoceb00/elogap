89f8c00a586d3c89530c56c4df60bd45bcf32ada
idoceb00 <idoceb00@estudiantes.unileon.es>
2026-02-13 18:04:04 +0100

    feat: add GET /v1/activities endpoint with basic filters

---
+++ api/src/main/java/com/idoceb00/elogap/http/controllers/ActivityController.java
@@ -0,0 +1,31 @@
+package com.idoceb00.elogap.http.controllers;
+
+import com.idoceb00.elogap.domain.Activity;
+import com.idoceb00.elogap.domain.MatchResult;
+import com.idoceb00.elogap.service.ActivityService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/v1/activities")
+public class ActivityController {
+
+    private final ActivityService service;
+
+    public ActivityController(ActivityService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Activity> list(
+            @RequestParam Optional<MatchResult> result,
+            @RequestParam Optional<String> champion
+    ){
+        return service.list(result, champion);
+    }
+}
